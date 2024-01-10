c2f4c34c5b6eccf0d6aaba4a47e2b22be6a8eb9a
Rahmadhany Triastanto <rahmadhany.triastanto@gmail.com>
2024-01-11 05:04:53 +0700

    setting a theme

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text('Fooderlich', style: theme.textTheme.titleLarge)),
+        body: Center(
+            child: Text(
+          'Let\'s get cooking 🧑‍🍳',
+          style: theme.textTheme.displayLarge,
+        )),
+      ),
+    );
+  }
+}
