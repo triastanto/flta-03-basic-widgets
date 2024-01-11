711f610dc29535561ac1ecb698b710421b41ae99
triastanto <rahmadhany.triastanto@gmail.com>
2024-01-11 10:01:41 +0700

    composing the chips

--- lib/card3.dart
+++ lib/card3.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:fooderlich/fooderlich_theme.dart';
 
 class Card3 extends StatelessWidget {
   const Card3({super.key});
@@ -20,9 +23,63 @@ class Card3 extends StatelessWidget {
         ),
         child: Stack(
           children: [
-            // TODO: Add dark overlay BoxDecoration
-            // TODO: Add Container, Column, Icon and Text
-            // TODO: Add Center widget with Chip widget children
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.6),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(10.0),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(
+                    Icons.book,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  const SizedBox(height: 8),
+                  Text('Recipe Trends',
+                      style: FooderlichTheme.darkTextTheme.displayMedium),
+                  const SizedBox(height: 30),
+                ],
+              ),
+            ),
+            Center(
+              child: Wrap(
+                alignment: WrapAlignment.start,
+                spacing: 12,
+                runSpacing: 12,
+                children: [
+                  Chip(
+                    label: Text(
+                      'Healthy',
+                      style: FooderlichTheme.darkTextTheme.bodyLarge,
+                    ),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: () => log('delete'),
+                  ),
+                  Chip(
+                    label: Text(
+                      'Vegan',
+                      style: FooderlichTheme.darkTextTheme.bodyLarge,
+                    ),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                    onDeleted: () => log('delete'),
+                  ),
+                  Chip(
+                    label: Text(
+                      'Vegan',
+                      style: FooderlichTheme.darkTextTheme.bodyLarge,
+                    ),
+                    backgroundColor: Colors.black.withOpacity(0.7),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
