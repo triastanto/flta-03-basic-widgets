795f77d46446ccb2f1558b55dca99e8203fe83d4
Rahmadhany Triastanto <rahmadhany.triastanto@gmail.com>
2024-01-11 07:00:12 +0700

    prepare author card

---
+++ lib/card2.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Card2 extends StatelessWidget {
+  const Card2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints.expand(
+          width: 350,
+          height: 450,
+        ),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/mag5.png'),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+        child: Column(
+          children: [
+            // TODO: Add author information
+            // TODO: Add Positioned text
+          ],
+        ),
+      ),
+    );
+  }
+}
