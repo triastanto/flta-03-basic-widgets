e6649993b948ca08e20b9365d2b9aafc6ab62192
triastanto <rahmadhany.triastanto@gmail.com>
2024-01-11 08:23:48 +0700

    adding the IconButton widget

--- lib/author_card.dart
+++ lib/author_card.dart
@@ -19,6 +19,7 @@ class AuthorCard extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(16),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Row(
             children: [
@@ -40,8 +41,16 @@ class AuthorCard extends StatelessWidget {
                   ),
                 ],
               ),
-              // TODO: Add IconButton
             ],
+          ),
+          IconButton(
+            icon: const Icon(Icons.favorite_border),
+            iconSize: 30,
+            color: Colors.grey[400],
+            onPressed: () {
+              const snackBar = SnackBar(content: Text('Favorite Pressed'));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            },
           ),
         ],
       ),
